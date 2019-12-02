5b1ec547f2235ba967ad2e08b79024656fcadf23
IKLOA <fandor1q@yandex.com>
2019-12-02 19:16:29 +0300

    implemented authorization / registration + passport js (backend)

--- server/app.js
+++ server/app.js
@@ -1,5 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const passport = require('passport')
+const keys = require('./config/keys')
 const app = express()
 
+const authRoutes = require('./routes/auth')
+
+mongoose.connect(keys.MongoDD)
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.log(error))
+
+
+app.use(passport.initialize())
+require('./middleware/pasport')(passport)
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
 
 module.exports = app
---
+++ server/controllers/auth.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../config/keys')
+const User = require('../models/User')
+const errorHandler = require('../utils/errorHandler')
+
+
+module.exports.login = async function (req, res) {
+  const candidate = await User.findOne({email: req.body.email})
+
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(req.body.password, candidate.password)
+    if (passwordResult) {
+      const token = jwt.sign({
+        email: candidate.email,
+        userId: candidate._id
+      }, keys.jwt, {expiresIn: 60 * 60})
+
+      res.status(200).json({
+        token: `Bearer ${token}`
+      })
+    } else {
+      res.status(401).json({
+        message: 'Вы ввели неверный пароль'
+      })
+    }
+  } else {
+    res.status(404).json({
+      message: 'Пользователь с таким email не найден'
+    })
+  }
+}
+
+module.exports.register = async function (req, res) {
+  const candidate = await User.findOne({email: req.body.email})
+
+  if (candidate) {
+    res.status(409).json({
+      message: 'Пользователь с такими email уже существует'
+    })
+  } else {
+    const salt = bcrypt.genSaltSync(10)
+    const password = req.body.password
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt)
+    })
+    try {
+      await user.save()
+      res.status(201).json(user)
+    } catch (e) {
+      errorHandler(res, e)
+    }
+
+  }
+
+
+}
